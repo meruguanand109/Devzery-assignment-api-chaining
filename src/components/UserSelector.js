4c236792a53e146136889a976b94d5346c7e0637
meruguanand109 <anandnandu109@gmail.com>
2024-10-09 14:01:28 +0530

    update 1

--- src/components/UserSelector.js
+++ src/components/UserSelector.js
@@ -1,18 +1,17 @@
 const UserSelector = ({ users, setSelectedUser }) => {
-    return (
-      <div className="user-selector">
-        <label>Select a User:</label>
-        <select onChange={(e) => setSelectedUser(users.find(user => user.id === e.target.value))}>
-          <option value="">Select User</option>
-          {users.map(user => (
-            <option key={user.id} value={user.id}>
-              {user.name}
-            </option>
-          ))}
-        </select>
-      </div>
-    );
-  };
-  
-  export default UserSelector;
-  
+  return (
+    <div className="user-selector">
+      <label>Select a User:</label>
+      <select onChange={(e) => setSelectedUser(users.find(user => user.id === e.target.value))}>
+        <option value="">Select User</option>
+        {users.map(user => (
+          <option key={user.id} value={user.id}>
+            {user.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default UserSelector;
