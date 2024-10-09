d05e81c3a2b669970349ae129c7db688a89e55ab
Anand Merugu <120920385+meruguanand109@users.noreply.github.com>
2024-10-09 14:26:56 +0530

    Update UserSelector.js

--- src/components/UserSelector.js
+++ src/components/UserSelector.js
@@ -2,7 +2,7 @@ const UserSelector = ({ users, setSelectedUser }) => {
   return (
     <div className="user-selector">
       <label>Select a User:</label>
-      <select onChange={(e) => setSelectedUser(users.find(user => user.id === e.target.value))}>
+      <select onChange={(e) => setSelectedUser(users.find(user => user.id == e.target.value))}>
         <option value="">Select User</option>
         {users.map(user => (
           <option key={user.id} value={user.id}>
